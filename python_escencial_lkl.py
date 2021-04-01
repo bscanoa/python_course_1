849d4c610c3be3264c0fdd5116b08ebb37140e4a
bscanoa <bscanoa@gmail.com>
2021-03-31 21:50:15 -0500

    Clase, Operadores lógicos. Clase, sentencia while

--- python_escencial_lkl.py
+++ python_escencial_lkl.py
@@ -7,7 +7,7 @@
 #y por último los de tipo booleano que tienen valores de falso o verdadero. 
 #También soporta números complejos.
 
-variable=valor_asignado
+#nombre_de_la_variable=valor_asignado
 
 ##Operadores matemáticos y comentarios.
 
@@ -29,7 +29,11 @@ print (2**3)
 ###Condicional IF
 ##Condición es una regla que se verifica y si se cumple toma un camino si no tomará otro.
 ##Sirven para tomar una descición.
-#estructura básica if(condicion)
+#estructura básica:
+#if(condicion):
+#   instrucciones si se cumple
+#else:
+#   instrucciones si no cumple
 
 edad=19
 pago=False
@@ -42,3 +46,44 @@ if edad>18:
         print("Eres mayor de edad y no pagaste")
 else:
     print ("No es mayor de edad")
+
+## Operadores relacionales en Python
+
+#Se encargan de la comparar de valores.
+# < menor que
+# <= menor o igual
+# > mayor que
+# >= mayor o igual
+# == para saber si son iguales
+# != para saber si son diferentes
+
+## Operadores lógicos
+
+# and  puede unir dos condiciones y verificarlas al mismo tiempo, se deben cumplir las dos condiciones.
+if edad>18 and edad<20:
+    print("Tu edad es 19")
+else:
+    print("Tu edad no es 19")
+# or solo es necesario que se cumpla una de las condiciones.
+if edad>18 or edad<20 or edad==24:
+    print("Tu edad es aceptada")
+else:
+    print("Tu edad no es aceptada")
+# not, se coloca antes de la condición 
+if not edad<18 :
+    print("Tu edad es aceptada")
+else:
+    print("Tu edad no es aceptada")
+
+##Sentencia while
+#Es un bucle "mientras", mietras se cumpla una condición las intrucciones se ejecutaran
+# while(este_lloviendo):
+#   usar_sombrilla
+
+numero=1
+
+while(numero<100):
+    print("El valor de número es:", numero)
+    numero=numero+1
+
+#Se ejecuta cuando sabemos lo que queremos o lo que no queremos.
