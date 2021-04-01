8b2de74e6aafdcd634dc3b993d98d2c2290c0694
bscanoa <bscanoa@gmail.com>
2021-03-31 20:24:54 -0500

    Clase de variables y tipos de datos

--- python_escencial_lkl.py
+++ python_escencial_lkl.py
@@ -1,1 +1,8 @@
-ls
+##Variables
+#Las variables almacenan datos que se van a manipular en memoria.
+#Estos datos pueden ser de diferentes tipos,
+#para el caso de python son: enteros, como la edad o números en general,
+#reales y flotantes como la estatura de alguien,
+#las cadenas como por ejemplo los nombre y van siempre entre comillas 
+#y por último los de tipo booleano que tienen valores de falso o verdadero. 
+#También soporta números complejos.
