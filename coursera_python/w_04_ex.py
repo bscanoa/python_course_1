bc03333cf58a12a04f31b67734c28ab12118f50d
bscanoa <bscanoa@gmail.com>
2021-04-01 22:29:51 -0500

    Ejercios y código de la semana 4. Convertir tipos de datos, solicitar información al usuario, operaciones

---
+++ coursera_python/w_04_ex.py
@@ -0,0 +1,24 @@
+# Semana 4 - Coursera - Python for Everybody
+#Convert elevator floors
+inp = input('European floor?')
+usf = int(inp) + 1
+print ('US floor', usf)
+
+
+#Cuando un flotante se convierte a entero, este se aproxima al valor entero.
+x=int(98.6)
+print(x) # x = 98
+
+#Ejercicio 2.2
+
+name = input("Enter your name:")
+print("Hello", name)
+
+#Ejercicio 2.3
+
+xh = input("Enter Hours: ")
+xr = input("Enter Rate: ")
+
+xp = float(xh) * float(xr)
+
+print("Pay:", xp)
