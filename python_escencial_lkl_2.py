8fd4baeda316d9b96b46926e6f006320af1d3a6a
bscanoa <bscanoa@gmail.com>
2021-04-02 15:57:07 -0500

    Tuplas, listas y diccionarios.

--- python_escencial_lkl_2.py
+++ python_escencial_lkl_2.py
@@ -56,3 +56,79 @@ def promedio(num1=0,num2=0,num3=0):
     resultado_p = (num1+num2+num3)/3
     return resultado_p
 print("El promedio es:", promedio(9,8,10))
+
+
+##Tuplas 
+# Las tuplas, nos permiten almacenar elementos de difertentes tipos
+# Las tuplas son inmutables.
+tupla=(25,1.73,"Pedro")
+
+# La lógica con la tupla trabaja es desde 0 hacia la izquirda. 
+print(tupla[2])
+
+indice=0
+
+#Para imprimir todos los elementos de una tupla.
+while indice<len(tupla):
+    print(tupla[indice])
+    indice=indice+1
+
+#Recorriendo una tupla con for
+tupla_1=(12,65,23,5,87)
+
+for numeros in tupla_1:
+    print (numeros)
+
+#Porciones de las tuplas
+tupla_2=(12,34,532,23,5,43,12,312,31)
+
+#Imprime los valores del 0 al 5 de la tupla anterior
+tupla_3=tupla_2[0:5] #Colocar el rango entre corchetes.
+
+print(tupla_3)
+
+# Manejo de cadenas en Python
+
+nombre_1="Un Gato Muy Robusto"
+
+print (nombre_1[:7])
+
+
+#Listas en Python
+# Tipos de datos similares a las tuplas. Pero en este caso si es posible cambiar los valores.
+
+lista=["Sara", "Pedro", 98, 76]
+lista_2=["Juan", "Castro", 89, 56]
+
+# Contando el número de elementos de una lista.
+print(len(lista))
+
+# Recorriendo listas con for.
+for item in lista:
+    print(item)
+
+# Concatenar listas.
+listaf=lista+lista_2
+print(listaf)
+
+# Verificación o busqueda de un elemento en una lista.
+if "Sora" in lista:
+    print("Nice")
+else:
+    print("NoNice")
+
+# Manejo de lista por porciones
+
+print(listaf[1:6])
+
+# Índices negativos en tuplas y diccionarios
+# Al poner un indice negativo recorrerá la lista de atras para adelante.
+
+print(listaf[-1])
+
+# Diccionario en Python
+
+diccionario={'José': 27, 'Rafael':90,'Sara':30, 'Luis':40}
+
+print(diccionario['Rafael'])
+print(len(diccionario))
