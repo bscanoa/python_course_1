66879f202777d5ae556f2eb3913846bd88d0573e
bscanoa <bscanoa@gmail.com>
2021-04-14 10:24:59 -0500

    W7 classes on w6

--- coursera_python/w_06_ex.py
+++ coursera_python/w_06_ex.py
@@ -35,19 +35,19 @@ def computepay(h,r):
 
 #Ver la solucion correcta esta abajo.
 
-hrs = input("Enter Hours: ")
-rat = input("Enter Rate: ")
-h = float(hrs)
-r = float(rat)
-p = computepay(h,r)
-print("Pay",p)
+#hrs = input("Enter Hours: ")
+#rat = input("Enter Rate: ")
+# h = float(hrs)
+# r = float(rat)
+# p = computepay(h,r)
+# print("Pay",p)
 
 #Solución correcta, uso adecuado de las variables.
 
 def computepay(hrs,rat):
    
-    hrs = input("Enter Hours: ")
-    rat = input("Enter Rate: ")
+    # hrs = input("Enter Hours: ")
+    # rat = input("Enter Rate: ")
     h = float(hrs)
     r = float(rat)
     
@@ -62,3 +62,70 @@ def computepay(hrs,rat):
 
 p = computepay(10,20)
 print("Pay",p)
+
+
+# Lasgest Value
+
+largest_so_far = 0
+print ('Before', largest_so_far)
+list1 =  [9,41,12,3,74,15]
+for the_num in list1:
+    if the_num > largest_so_far :
+        largest_so_far = the_num
+    print (largest_so_far, the_num)
+print ('After', largest_so_far)
+
+# Counting
+zork = 0
+print('Before', zork)
+for thing in list1 :
+    zork = zork + 1
+    print(zork, thing)
+print('After', zork)
+
+# Summing in a loop
+zork = 0
+print('Before', zork)
+for thing in list1 :
+    zork = zork + thing
+    print(zork, thing)
+print('After', zork)
+
+# Finding the Average
+count = 0 
+sum = 0
+
+print('Before', count, sum)
+for value in list1:
+    count = count + 1
+    sum = sum + value
+print('After', count, sum, int(sum/count))
+
+# Filtering in a Loop
+
+print('Before')
+for value in list1:
+    if value>20:
+        print('Large number', value)
+print('After')
+
+# Search Using a Boolean Variable
+
+found = False
+print('Before', found)
+for value in list1:
+    if value==3:
+        found = True 
+        break
+    print (found, value)
+print('After', found)
+
+# Smallest
+smallest_so_far = None
+for value in list1:
+    if smallest_so_far is None:
+        smallest_so_far = value
+    elif value < smallest_so_far :
+        smallest_so_far = value
+    print (smallest_so_far, value)
+print ('After', smallest_so_far)
